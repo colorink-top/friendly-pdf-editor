96e50b48c35c6ab51b07a8bdf339cdc8663dbf52
suziwen <suziwen1@gmail.com>
2024-01-18 12:25:51 +0800

    release

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/friendly-pdf-editor/sw.js', { scope: '/friendly-pdf-editor/' })})}
